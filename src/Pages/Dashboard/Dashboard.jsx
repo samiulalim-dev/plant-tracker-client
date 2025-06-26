dd25558a21fdfcad9d31165f821905d36193b6d0
samiul-alim12 <nawabitsamiul@gmail.com>
2025-06-27 03:57:39 +0600

    added dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -9,50 +9,51 @@ const Dashboard = () => {
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content mx-5 mt-5">
+        <div className="navbar bg-base-300 w-full">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <Link to="/dashboard" className="mx-1 px-1 flex-1">
+            Dashboard
+          </Link>
+
+          <div className="hidden flex-none lg:hidden">
+            <ul className="menu menu-horizontal">
+              {/* Navbar menu content here */}
+              <li>
+                <a>Navbar Item 1</a>
+              </li>
+              <li>
+                <a>Navbar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         {/* Page content here */}
         <Outlet></Outlet>
         <div className="drawer">
           <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex lg:hidden flex-col">
             {/* Navbar */}
-            <div className="navbar bg-base-300 w-full">
-              <div className="flex-none lg:hidden">
-                <label
-                  htmlFor="my-drawer-3"
-                  aria-label="open sidebar"
-                  className="btn btn-square btn-ghost"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block h-6 w-6 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    ></path>
-                  </svg>
-                </label>
-              </div>
-              <Link to="/dashboard" className="mx-1 px-1 flex-1">
-                Dashboard
-              </Link>
 
-              <div className="hidden flex-none lg:hidden">
-                <ul className="menu menu-horizontal">
-                  {/* Navbar menu content here */}
-                  <li>
-                    <a>Navbar Item 1</a>
-                  </li>
-                  <li>
-                    <a>Navbar Item 2</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
             {/* Page content here */}
           </div>
 
